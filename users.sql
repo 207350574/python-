d0c7250341fe925d8432fed8de7ac791194ce89f
207350574 <104699858+207350574@users.noreply.github.com>
2024-12-29 18:25:21 +0800

    users.sql

---
+++ users.sql
@@ -0,0 +1,36 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost_3306
+ Source Server Type    : MySQL
+ Source Server Version : 80037 (8.0.37)
+ Source Host           : localhost:3306
+ Source Schema         : users
+
+ Target Server Type    : MySQL
+ Target Server Version : 80037 (8.0.37)
+ File Encoding         : 65001
+
+ Date: 29/12/2024 17:20:10
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of users
+-- ----------------------------
+INSERT INTO `users` VALUES (6, 'admin', 'admin');
+
+SET FOREIGN_KEY_CHECKS = 1;
